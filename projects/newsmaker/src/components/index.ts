88b95a4ea9d359174122648bfda12235b4ddc3fc
vitya3000-inn <obmornov.v@gmail.com>
2026-03-27 17:13:41 +0300

    Update index.ts

--- projects/newsmaker/src/components/index.ts
+++ projects/newsmaker/src/components/index.ts
@@ -1,1 +1,7 @@
-
+export { default as Header } from './Header';
+export { default as TopicCard } from './TopicCard';
+export { default as EventCard } from './EventCard';
+export { default as OpinionCard } from './OpinionCard';
+export { default as OpinionMap } from './OpinionMap';
+export { default as CategoryBadge } from './CategoryBadge';
+export { default as FilterChips } from './FilterChips';
